5947a34aa85569d81a2a6e9b4c8d3b277853c6b4
PaulaChrustowska <paula.chrustowska@interia.pl>
2022-01-21 18:05:21 +0100

    BlogList

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -1,6 +1,13 @@
-function BlogList() {
+import React from 'react';
+
+function BlogList({ blogs, title, greet }) {
+  // const { blogs } = props;
+  // const { title } = props;
+
   return (
     <div className="blog-list">
+      <h2>{ title }</h2>
+      <h2>{ greet }</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
@@ -13,3 +20,5 @@ function BlogList() {
     </div>
   );
 }
+
+export default BlogList;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
+import BlogList from './BlogList';
 
 function Home() {
   const [blogs] = useState([
     {
-      title: 'My new website', body: 'Witajcie! W naszej bajce Słoń zagra na fujarce, Pinokio nam zaśpiewa, Zatańczą wkoło drzewa. Tu wszystko jest możliwe,Zwierzęta są szczęśliwe, A dzieci, wiem coś o tym,Latają samolotem.Nikt tutaj nie zna głodu,Nikt tu nie czuje chłodu I nawet - ja nie kłamię Nikt się nie skarży mamie', author: 'Jan Brzechwa', id: 1,
+      title: 'Inrroduce', body: 'Witajcie! W naszej bajce Słoń zagra na fujarce, Pinokio nam zaśpiewa, Zatańczą wkoło drzewa. Tu wszystko jest możliwe,Zwierzęta są szczęśliwe, A dzieci, wiem coś o tym,Latają samolotem.Nikt tutaj nie zna głodu,Nikt tu nie czuje chłodu I nawet - ja nie kłamię Nikt się nie skarży mamie', author: 'Jan Brzechwa', id: 1,
     },
     { title: 'How to make the perfect cake..', author: 'Jaś Fasola', id: 2 },
     {
@@ -13,9 +14,14 @@ function Home() {
 
   return (
     <div className="home">
-      <BlogList />
+      <BlogList blogs={blogs} title="All Blogs!" greet="Hello! Welcome to our world!" />
     </div>
   );
 }
+/*
+
+Czarno, czarniej, Czarnek
+
+*/
 
 export default Home;
