68fc2ba8921e6b9484961867d08a836934492d5e
PaulaChrustowska <paula.chrustowska@interia.pl>
2022-01-24 15:44:09 +0100

    reusing

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,7 @@ import BlogList from './BlogList';
 function Home() {
   const [blogs] = useState([
     {
-      title: 'Inrroduce', body: 'Witajcie! W naszej bajce Słoń zagra na fujarce, Pinokio nam zaśpiewa, Zatańczą wkoło drzewa. Tu wszystko jest możliwe,Zwierzęta są szczęśliwe, A dzieci, wiem coś o tym,Latają samolotem.Nikt tutaj nie zna głodu,Nikt tu nie czuje chłodu I nawet - ja nie kłamię Nikt się nie skarży mamie', author: 'Jan Brzechwa', id: 1,
+      title: 'Introduce', body: 'Witajcie! W naszej bajce Słoń zagra na fujarce, Pinokio nam zaśpiewa, Zatańczą wkoło drzewa. Tu wszystko jest możliwe,Zwierzęta są szczęśliwe, A dzieci, wiem coś o tym,Latają samolotem.Nikt tutaj nie zna głodu,Nikt tu nie czuje chłodu I nawet - ja nie kłamię Nikt się nie skarży mamie', author: 'Jan Brzechwa', id: 1,
     },
     { title: 'How to make the perfect cake..', author: 'Jaś Fasola', id: 2 },
     {
@@ -15,6 +15,8 @@ function Home() {
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs!" greet="Hello! Welcome to our world!" />
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'Jaś Fasola')} title="John Blog" greet="Hello! Welcome to my world!" />
+
     </div>
   );
 }
