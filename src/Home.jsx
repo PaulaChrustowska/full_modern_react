3c5190c5a81e11c9fd13886e0a73ece161d410fa
PaulaChrustowska <paula.chrustowska@interia.pl>
2022-01-28 15:58:36 +0100

    conditional loading message

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,17 +3,20 @@ import BlogList from './BlogList';
 
 function Home() {
   const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
       .then((res) => res.json())
       .then((data) => {
         setBlogs(data);
+        setIsLoading(false);
       });
   }, []);
 
   return (
     <div className="home">
+      { isLoading && <div>One moment...</div> }
       {blogs && <BlogList blogs={blogs} title="All Blogs!" greet="Hello! Welcome to our world!" />}
     </div>
   );
