4cdd6679bf7b08028bad73b1c03752df60a6c7c9
PaulaChrustowska <paula.chrustowska@interia.pl>
2022-02-01 12:36:10 +0100

    handling errors

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,19 +4,31 @@ import BlogList from './BlogList';
 function Home() {
   const [blogs, setBlogs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) {
+          throw Error('oooops, could not fetch the data');
+        }
+        return res.json();
+      })
       .then((data) => {
+        setIsLoading(false);
         setBlogs(data);
         setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
       });
-  }, []);
+  }, 1000);
 
   return (
     <div className="home">
-      { isLoading && <div>One moment...</div> }
+      {error && <div>{ error }</div> }
+      {isLoading && <div>One moment...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" greet="Hello! Welcome to our world!" />}
     </div>
   );
