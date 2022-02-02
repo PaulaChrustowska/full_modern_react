e4f904307751fba6e9ee4c0fe9a5a3e0aae77db9
PaulaChrustowska <paula.chrustowska@interia.pl>
2022-02-02 10:21:45 +0100

    creating a cutom hook

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,42 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 function Home() {
-  const [blogs, setBlogs] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then((res) => {
-        if (!res.ok) {
-          throw Error('oooops, could not fetch the data');
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setIsLoading(false);
-        setBlogs(data);
-        setIsLoading(false);
-        setError(null);
-      })
-      .catch((err) => {
-        setError(err.message);
-      });
-  }, 1000);
+  const { error, isLoading, data: blogs } = useFetch('http://localhost:8000/blogs');
 
   return (
     <div className="home">
-      {error && <div>{ error }</div> }
-      {isLoading && <div>One moment...</div>}
-      {blogs && <BlogList blogs={blogs} title="All Blogs!" greet="Hello! Welcome to our world!" />}
+      {error && <div>{error}</div>}
+      {isLoading && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} />}
     </div>
   );
 }
-/*
-
-Czarno, czarniej, Czarnek
-
-*/
 
 export default Home;
