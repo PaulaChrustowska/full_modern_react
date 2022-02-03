b3b278963ca3a8e0e70b5f4efd98e41b7c635e2a
PaulaChrustowska <paula.chrustowska@interia.pl>
2022-02-03 20:13:17 +0100

    controlled Inputs (forms)

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Create() {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('Jaś Fasola');
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+        <label>Blog author:</label>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="Jaś Fasola">Jaś Fasola</option>
+          <option value="James Bond">James Bond</option>
+        </select>
+        <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{ body }</p>
+        <p>{ author }</p>
+      </form>
     </div>
   );
 }
