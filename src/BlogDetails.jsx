31a8783f1af70ff9775a58de9770ac756b783994
PaulaChrustowska <paula.chrustowska@interia.pl>
2022-02-07 13:56:47 +0100

    deleting blogs

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import useFetch from './useFetch';
 
 function BlogDetails() {
   const { id } = useParams();
   const { data: blog, error, isLoading } = useFetch(`http://localhost:8000/blogs/${id}`);
+  const history = useHistory();
+
+  const handleClick = () => {
+    fetch(`http://localhost:8000/blogs/${blog.id}`, {
+      method: 'DELETE',
+    }).then(() => {
+      history.push('/');
+    });
+  };
 
   return (
     <div className="blog-details">
@@ -19,6 +28,7 @@ function BlogDetails() {
           { blog.author }
         </p>
         <div>{ blog.body }</div>
+        <button onClick={handleClick}>delete</button>
       </article>
       )}
     </div>
